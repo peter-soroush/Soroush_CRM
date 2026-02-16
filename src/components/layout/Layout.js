4d8ddd914c5a682631c50516d1ca40d7b64a8e04
Peter Soroush <peter.soroush@gmail.com>
2026-02-16 14:30:12 +0330

    Database Created and its validation has been written and tested

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,9 +1,16 @@
-import React from "react";
+import Link from "next/link";
 
 function Layout({ children }) {
   return (
     <>
-      <header clasName="header"></header>
+      <header className="header">
+        <h2>Soroush CRM</h2>
+        <Link href="/add-customer">Add Customer</Link>
+      </header>
+      <main className="main">{children}</main>
+      <footer className="footer">
+        <a>ArSoroush | NextJs Course | CRM Project &copy; 2026 </a>
+      </footer>
     </>
   );
 }
---
+++ src/pages/api/customer/index.js
@@ -0,0 +1,40 @@
+import Customer from "../../../../models/Customer";
+import connectDB from "../../../../utils/connectDB";
+
+export default async function handler(req, res) {
+  try {
+    await connectDB();
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: "failed",
+      message: "Something went wrong related to DB connection",
+      error: err.message,
+    });
+    return;
+  }
+
+  if (req.method === "POST") {
+    const data = req.body.data;
+
+    if (!data.name || !data.LastName || !data.email) {
+      return res.status(400).json({
+        status: "failed",
+        message: "Missing required fields",
+      });
+    }
+    try {
+      const customer = await Customer.create(data);
+      res.status(201).json({
+        status: "success",
+        message: "Data Created Successfully",
+        data: customer,
+      });
+    } catch (err) {
+      console.log(err);
+      res
+        .status(500)
+        .json({ status: "failed", message: "Error in storing Data in DB" });
+    }
+  }
+}
